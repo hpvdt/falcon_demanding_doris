bfd6a1b3d4af56a5b950f6210430ad0c3eada898
alyaseker <alya.seker@mail.utoronto.ca>
2023-09-16 15:13:29 -0400

    changed frequency

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 
+// read HX711
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PB3, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PB3, HIGH); 
-  delay(500); 
-  digitalWrite(PB3, LOW); 
-  delay(500); 
+  digitalWrite(3, HIGH); 
+  delay(40); 
+  digitalWrite(3, LOW); 
+  delay(40); 
 }
 
