bea9edca66bc2dad7c9cd3be3e588b24a3cc7b66
ishi <113075831+Hypertrrr@users.noreply.github.com>
2023-09-16 14:50:45 -0400

    added flash code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(PB3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(PB3, HIGH); 
+  delay(500); 
+  digitalWrite(PB3, LOW); 
+  delay(500); 
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
