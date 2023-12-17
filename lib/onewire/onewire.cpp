d779fca4ccb2436b19042fbc63e97792fce78eed
Savo Bajic <bajic.savo@gmail.com>
2023-12-17 18:52:00 -0500

    Made a `sendData` function for one wire
    Should make changes to how the data is sent easier to implement

--- lib/onewire/onewire.cpp
+++ lib/onewire/onewire.cpp
@@ -5,6 +5,7 @@
 #include "onewire.hpp"
 
 void handleOneWireInput(); // Since it is only meant to be used as an interrupt it is locally scoped
+void sendData(uint32_t data, uint8_t width);
 
 const uint8_t bitPeriod = 50; // Period for each bit in us
 const uint8_t addressWidth = 4; // Number of bits for device addresses
@@ -77,19 +78,7 @@ void handleOneWireInput() {
     delayMicroseconds(bitPeriod / 2);
 
     // Send out the data
-    const int32_t maskedBitToSend = 1L << (dataWidth - 1);
-
-    for (uint8_t i = 0; i < dataWidth; i++) {
-        bool makeLineActive = ((maskedBitToSend & (oneWirePayload << i)) != 0);
-
-        // Since the output gets inverter due to the inverter the opposite line active state is written out
-        if (makeLineActive) digitalWrite(pinTX, !lineActive);
-        else digitalWrite(pinTX, lineActive);
-
-        delayMicroseconds(bitPeriod - delayLag);
-    }
-
-    digitalWrite(pinTX, LOW); // Release line
+    sendData(oneWirePayload, dataWidth);
 }
 
 /**
@@ -109,18 +98,9 @@ void requestOneWire(uint8_t targetAdd, int32_t *destination) {
     delayMicroseconds(bitPeriod / 2 - delayLag);
 
     // Send out address
-    const int32_t maskedBitToSend = 1 << (addressWidth - 1);
-    for (uint8_t i = 0; i < addressWidth; i++) {
-        bool makeLineActive = ((maskedBitToSend & (targetAdd << i)) != 0);
+    sendData(targetAdd, addressWidth);
 
-        // Since the output gets inverter due to the inverter the opposite line active state is written out
-        if (makeLineActive) digitalWrite(pinTX, !lineActive);
-        else digitalWrite(pinTX, lineActive);
-
-        delayMicroseconds(bitPeriod - delayLag);
-    }
-    digitalWrite(pinTX, LOW); // Release line to called device
-
+    // Read data in from line
     for (uint8_t i = 0; i < dataWidth; i++) {
         *(destination) = *(destination) << 1;
         if (digitalRead(pinRX) == lineActive) *(destination) = *(destination) + 1;
@@ -133,6 +113,37 @@ void requestOneWire(uint8_t targetAdd, int32_t *destination) {
     }
 }
 
+/**
+ * @brief Send data over one wire interface
+ * 
+ * @note Shifts data out MSB first
+ * 
+ * @param data Payload to send
+ * @param width The width of the data to send in bits
+ */
+void sendData(uint32_t data, uint8_t width) {
+    noInterrupts(); // Don't want interrupts to catch outgoing message
+
+    uint32_t mask = 1 << (width - 1);
+    for (uint8_t i = 0; i < addressWidth; i++) {
+        bool makeLineActive = ((mask & (data << i)) != 0);
+
+        // Since the output gets inverter due to the inverter the opposite line active state is written out
+        if (makeLineActive) digitalWrite(pinTX, !lineActive);
+        else digitalWrite(pinTX, lineActive);
+
+        delayMicroseconds(bitPeriod - delayLag);
+    }
+
+    digitalWrite(pinTX, LOW); // Release line
+    interrupts();
+}
+
+/**
+ * @brief Set the one wire response payload
+ * 
+ * @param newPayload What to repsond with next one wire query
+ */
 void setPayload(int32_t newPayload) {
     noInterrupts();
     oneWirePayload = newPayload;
